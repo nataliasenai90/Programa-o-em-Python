bbc87e6fed87c60d616ceb57a029561940060c21
nataliasenai90 <natalia.senai90@gmail.com>
2026-06-17 14:50:43 -0300

    Add files via upload

---
+++ atividade 5.py
@@ -0,0 +1,40 @@
+# 1
+print("Seja bem-vindo!")
+
+#2
+a = True
+print(a)
+
+#3
+multiplicacao = 5.3 * 9.6
+print(multiplicacao)
+
+#4
+divisao = 10 / 2
+print(divisao)
+
+#5
+subtracao = 9 - 7
+print(subtracao)
+
+#6
+div_inteira = 20 // 6
+print(div_inteira)
+
+#7
+multiplicacao2 = 6.7 * 4.3 * 1.2 * 7.9
+print(multiplicacao2)
+
+#8
+numero = 18
+print(numero * 2)
+
+#9
+nome = input("Nome: ")
+email = input("E-mail: ")
+idade = input("Idade: ")
+cidade = input("Cidade: ")
+graduacao = input("Graduação: ")
+estado_civil = input("Estado civil: ")
+
+print(f"{nome}\n{email}\n{idade}\n{cidade}\n{graduacao}\n{estado_civil}")
